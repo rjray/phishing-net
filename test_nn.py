3cd254fda2bdfceeb6b85972cc85a98414982b6a
Randy J. Ray <rjray@blackperl.com>
2022-05-05 19:30:05 -0700

    Simple script to test NeuralNetwork class

---
+++ test_nn.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+_root_dir = os.path.dirname(__file__)
+sys.path.append(_root_dir)
+
+import numpy as np
+
+from phishing.classifier.neural_net import NeuralNetwork
+from phishing.classifier.nn_layer.perceptron import Perceptron
+
+# training data
+x_train = np.array([[[0, 0]], [[0, 1]], [[1, 0]], [[1, 1]]])
+y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
+
+# net = NeuralNetwork()
+# net.add(Perceptron(2, 3))
+# net.add(Perceptron(3, 1))
+
+net = NeuralNetwork(
+    Perceptron(2, 3),
+    Perceptron(3, 1)
+)
+
+net.fit(x_train, y_train)
+
+print(net.predict(x_train))
