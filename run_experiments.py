b738b021985ca6569259af3e58e0f161a79e419f
Randy J. Ray <rjray@blackperl.com>
2022-05-10 00:04:42 -0700

    Checkpoint again

--- run_experiments.py
+++ run_experiments.py
@@ -6,10 +6,6 @@ import sys
 _root_dir = os.path.dirname(__file__)
 sys.path.append(_root_dir)
 
-# TODO: Remove
-# from pprint import PrettyPrinter
-# PP = PrettyPrinter(indent=2)
-
 import argparse
 import math
 import matplotlib.pyplot as plt
@@ -24,6 +20,13 @@ from phishing.classifier.logistic import LogisticRegression
 from phishing.classifier.neural_net import NeuralNetwork
 from phishing.classifier.nn_layer.perceptron import Perceptron
 from phishing.data.dataset import Dataset
+
+
+descriptions = {
+    "all": "full dataset",
+    "no_3rd": "no third-party tool data",
+    "url_only": "URL-derived data only",
+}
 
 
 def parse_command_line():
@@ -99,62 +102,92 @@ def determinization_points(labels, positives, N=1000):
     return points
 
 
-def run_one_model_data_combo(type, datatype, instance, ds, alphas):
+def run_one_model_data_combo(type, datatype, instance, ds, alpha):
     # Unlike the homework assignments, here we aren't fishing around for ideal
     # parameters before running the test evaluation. Rather, we will
     # immediately fit the model to the combined train/validation data and base
     # the stats and the plots on the test data.
-
-    print(f"Processing {type} model with {datatype} dataset.")
-    start = timer()
 
     X_base = ds.X_base
     y_base = ds.y_base
     X_test = ds.X_test
     y_test = ds.y_test
 
-    results = []
+    stats_data = {
+        "alpha": alpha,
+        "type": type,
+        "dataset": datatype
+    }
 
-    for alpha in alphas:
-        stats_data = {
-            "alpha": alpha,
-            "type": type,
-            "dataset": datatype
-        }
+    indent = "    "
+    if type.startswith("Neural"):
+        indent += "  "
 
-        print(f"  α={alpha}")
-        print("    Fitting base data")
-        model = instance.fit(X_base, y_base, alpha=alpha)
-        print("    Getting probabilities and prediction")
-        probabilities = model.predict_proba(X_test)
-        prediction = model.predict(X_test)
-        print("    Doing determinization for ROC curve")
-        points = determinization_points(y_test, probabilities)
-        curve_pts = sorted([[x, y] for _, x, y in points], key=itemgetter(0))
-        print("    Calculating best Youden Index")
-        youden = sorted(
-            [((tpr - fpr), t) for t, fpr, tpr in points],
-            key=itemgetter(0), reverse=True
-        )[0]
+    print(f"{indent}α={alpha}")
+    print(f"{indent}  Fitting base data")
+    model = instance.fit(X_base, y_base, alpha=alpha)
+    print(f"{indent}  Getting probabilities and prediction")
+    probabilities = model.predict_proba(X_test)
+    prediction = model.predict(X_test)
+    print(f"{indent}  Doing determinization for ROC curve")
+    points = determinization_points(y_test, probabilities)
+    curve_pts = sorted([[x, y] for _, x, y in points], key=itemgetter(0))
+    print(f"{indent}  Calculating best Youden Index")
+    youden = sorted(
+        [((tpr - fpr), t) for t, fpr, tpr in points],
+        key=itemgetter(0), reverse=True
+    )[0]
 
-        # The NeuralNetwork class will return a Mx1-shaped column vector. If
-        # we have that, reshape it to a row vector of dimension M.
-        if len(prediction.shape) == 2 and prediction.shape[1] == 1:
-            prediction = prediction.ravel()
+    # The NeuralNetwork class will return a Mx1-shaped column vector. If
+    # we have that, reshape it to a row vector of dimension M.
+    if len(prediction.shape) == 2 and prediction.shape[1] == 1:
+        prediction = prediction.ravel()
 
-        stats_data["auc"] = roc_auc_score(y_test, probabilities)
-        stats_data["risk"] = np.sum(np.abs(prediction - y_test)) / y_test.size
-        stats_data["score"] = 1 - stats_data["risk"]
-        stats_data["youden"] = youden
-        stats_data["curve_pts"] = curve_pts
-        stats_data["caption"] = f"{type} (data={datatype}, α={alpha})"
-        # PP.pprint(stats_data)
+    stats_data["auc"] = roc_auc_score(y_test, probabilities)
+    stats_data["risk"] = np.sum(np.abs(prediction - y_test)) / y_test.size
+    stats_data["score"] = 1 - stats_data["risk"]
+    stats_data["youden"] = youden
+    stats_data["curve_pts"] = curve_pts
+    stats_data["caption"] = f"{type} (data={datatype}, α={alpha})"
 
-        results.append(stats_data)
+    return stats_data
 
-    end = timer()
-    print(f"Processing {type} complete ({elapsed(start, end)}).")
-    return results
+
+def plot_curve(ax, points, label, color):
+    pts = np.array(points)
+    ax.plot(pts[:, 0], pts[:, 1], lw=1, color=color, label=label)
+
+    return
+
+
+def generate_plots(data, title, filename):
+    # Colors we'll use:
+    colors = ["red", "green", "blue"]
+
+    # Set up the 2x2 grid of plots.
+    fig, axs = plt.subplots(2, 2)
+    fig.suptitle(title)
+
+    for idx, run in enumerate(data):
+        i = 1 if idx & 2 else 0
+        j = 1 if idx & 1 else 0
+        label = f"α={run['alpha']}"
+        plot_curve(axs[i][j], run["curve_pts"], label, colors[idx])
+        plot_curve(axs[1][1], run["curve_pts"], label, colors[idx])
+
+    for i in range(2):
+        for j in range(2):
+            axs[i][j].plot(
+                [0, 1], [0, 1], color="navy", lw=1, linestyle="--", marker="."
+            )
+            axs[i][j].set_xlim([-0.05, 1.05])
+            axs[i][j].set_ylim([-0.05, 1.05])
+            axs[i][j].grid()
+            axs[i][j].legend(loc="lower right")
+
+    fig.savefig(filename)
+
+    return
 
 
 def main():
@@ -182,26 +215,43 @@ def main():
     for d in ds:
         d.create_split(0.2, random_test=seed, random_validate=seed)
 
+    # Gather all the data from all the runs:
+    data = []
+
     # Start gathering data.
     run_started = timer()
 
     # Obtain data for the logistic regression models:
-    lr_data = []
+    print("Processing LogisticRegression classifier")
+    start = timer()
     for ds_index in range(3):
-        lr_data.append(
-            run_one_model_data_combo(
-                "LogisticRegression",
-                ds_labels[ds_index],
-                LogisticRegression(),
-                ds_bias[ds_index],
-                alphas
+        print(f"  Using dataset {ds_labels[ds_index]}")
+        row = []
+        ds_start = timer()
+        for alpha in alphas:
+            row.append(
+                run_one_model_data_combo(
+                    "LogisticRegression",
+                    ds_labels[ds_index],
+                    LogisticRegression(),
+                    ds_bias[ds_index],
+                    alpha
+                )
             )
-        )
+        ds_end = timer()
+        data.append(row)
+        print(f"  Dataset completed ({elapsed(ds_start, ds_end)})")
+    end = timer()
+    print(f"LogisticRegression processing complete ({elapsed(start, end)})")
 
     # Now gather for the neural network models. Note that each dataset calls
     # for slightly different NN instances.
-    nn_data = []
+    print("Processing NeuralNetwork classifier")
+    start = timer()
     for ds_index in range(3):
+        print(f"  Using dataset {ds_labels[ds_index]}")
+        ds_start = timer()
+
         dataset = ds[ds_index]
         # Each of the three datasets is a different size
         features = dataset.X.shape[1]
@@ -235,22 +285,42 @@ def main():
             )
         )
 
-        row = []
         for nn_index in range(3):
-            row.append(
-                run_one_model_data_combo(
-                    labels[nn_index],
-                    ds_labels[ds_index],
-                    nn_list[nn_index],
-                    dataset,
-                    alphas
+            print(f"    Using {labels[nn_index]}")
+            row = []
+            for alpha in alphas:
+                row.append(
+                    run_one_model_data_combo(
+                        labels[nn_index],
+                        ds_labels[ds_index],
+                        nn_list[nn_index],
+                        dataset,
+                        alpha
+                    )
                 )
-            )
+                nn_list[nn_index].reset()
+            print(f"    {labels[nn_index]} complete")
+            data.append(row)
 
-        nn_data.append(row)
+        ds_end = timer()
+        print(f"  Dataset completed ({elapsed(ds_start, ds_end)})")
+
+    end = timer()
+    print(f"NeuralNetwork processing complete ({elapsed(start, end)})")
 
     run_finished = timer()
-    print(f"Total data-time: {elapsed(run_started, run_finished)}")
+    print(f"\nTotal data-time: {elapsed(run_started, run_finished)}")
+
+    print("\nCreating plots")
+    for row in data:
+        type = row[0]["type"]
+        dataset = row[0]["dataset"]
+        ds_desc = descriptions[dataset]
+
+        title = f"{type} on {ds_desc}"
+        filename = f"{type.lower()}-{dataset}.png"
+        print(f"  writing {filename}")
+        generate_plots(row, title, filename)
 
     return
 
